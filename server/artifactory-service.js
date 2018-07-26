68e8ec88e307340630c81bfc82d9e9180b4beb50
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-07-26 20:24:54 +0200

    simplify server api

--- server/artifactory-service.js
+++ server/artifactory-service.js
@@ -3,9 +3,10 @@ const axios = require("axios");
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 axios.defaults.baseURL = 'http://artifactory.init.de/artifactory/api/npm/';
 axios.defaults.headers.common['Authorization'] = "AKCp5bB3N7rCWCeSyuh64ehyYQP6HGLAZ9Dc3UhhVKL1hyskWGrjprmxAKfKZoDdf9XfQpVo4";
+const repoKey = 'npm';
 
 module.exports = {
   fetchPackages: async () => {
-    return await axios.get('npm-local/-/all');
+    return await axios.get(`${repoKey}/-/all`);
   }
 };
