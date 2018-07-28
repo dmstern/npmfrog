b903e75c7c40d031b20e20607abb6a7661f55da8
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-07-28 12:26:41 +0200

    Add server mock

--- server/artifactory-service.js
+++ server/artifactory-service.js
@@ -1,36 +1,23 @@
 const axios = require("axios");
-const path = require('path');
-const homedir = require('os').homedir();
-const config = require(path.join(homedir, '.npmfrog.config.json'));
+const path = require("path");
+const homedir = require("os").homedir();
+const config = require(path.join(homedir, ".npmfrog.config.json"));
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 axios.defaults.baseURL = `${config.artifactory.baseURL}/artifactory/api/npm/`;
-axios.defaults.headers.common['Authorization'] = config.artifactory.apiKey;
+axios.defaults.headers.common["Authorization"] = config.artifactory.apiKey;
 
 const repoKey = config.artifactory.repoKey;
 
 module.exports = {
   fetchPackages: async () => {
-    const request = axios.get(`${repoKey}/-/all`);
-    return request;
-    // return new Promise((resolve, reject) => {
-    //   resolve({data: {
-    //     module1: {
-    //       name: "hello",
-    //       description: "Beschreibung",
-    //       author: "icke eben"
-    //     },
-    //     module2: {
-    //       name: "hello",
-    //       description: "Beschreibung",
-    //       author: "icke eben"
-    //     },
-    //     module3: {
-    //       name: "hello",
-    //       description: "Beschreibung",
-    //       author: "icke eben"
-    //     }
-    //   }});
-    // });
+    if (process.env.MOCK) {
+      return new Promise((resolve, reject) => {
+        resolve({
+          data: require('./mock/packages-all.json')
+        });
+      });
+    }
+    return (request = axios.get(`${repoKey}/-/all`));
   }
 };
