a0865bf97952dc71ecf30f82ebfff3720dacc733
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-07-30 23:56:30 +0200

    Add theme colors / style autocomplete box

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 
 <div class="page-container">
   <md-app id="app" md-waterfall md-mode="fixed">
-    <md-app-toolbar class="md-primary">
+    <md-app-toolbar class="md-white">
       <div class="md-toolbar-row">
         <div class="md-toolbar-section-start">
           <md-button class="md-icon-button" @click="menuVisible = !menuVisible">
@@ -74,13 +74,13 @@
 
 <style lang="scss">
 @import url("//fonts.googleapis.com/css?family=Roboto:400,500,700,400italic|Fira+Mono|Material+Icons");
-@import 'assets/variables';
+@import "assets/variables";
 @import "~vue-material/dist/theme/engine";
 
-@include md-register-theme("default", (
-  primary: white,
-  accent: #D63E19
-));
+@include md-register-theme(
+  "default",
+  (primary: md-get-palette-color(blue, A200), accent: #d63e19, neutral: white)
+);
 
 @import "~vue-material/dist/theme/all"; // Apply the theme
 
@@ -92,17 +92,51 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   height: 100%;
+
+  .md-toolbar {
+    border-bottom: 1px solid $color-gray-extralight;
+
+    @include md-theme-component() {
+      &.md-white {
+        @include md-theme-property(background-color, neutral);
+        @include md-theme-property(color, text-primary, neutral);
+        @include md-toolbar-icon(text-accent, neutral);
+        @include md-toolbar-button(text-primary, neutral);
+        @include md-toolbar-title(text-primary, neutral);
+      }
+    }
+
+    &.md-white {
+      background-color: white;
+    }
+  }
+}
+
+.md-toolbar .md-autocomplete.md-theme-default.md-autocomplete-box {
+  label,
+  input {
+    @include md-theme-property(color, text-hint, neutral);
+    @include md-theme-property(-webkit-text-fill-color, text-hint, neutral);
+  }
+
+  &.md-focused {
+    label,
+    input {
+      @include md-theme-property(color, text-accent, neutral);
+      @include md-theme-property(-webkit-text-fill-color, text-accent, neutral);
+    }
+  }
 }
 
 .md-badge.md-square.md-theme-default.search-key {
   background-color: $color-gray-light;
   display: inline-block;
-  padding: .3em;
-  margin-right: .3em;
+  padding: 0.3em;
+  margin-right: 0.3em;
   min-height: 1rem;
 
   &::after {
-    content: ':';
+    content: ":";
   }
 
   &.author {
@@ -117,7 +151,6 @@
 .search-item--with-key {
   display: flex;
 }
-
 </style>
 
 <script lang="ts">
