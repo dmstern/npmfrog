4deeae1be34907f78a9ad52c08ea3d8b53b4ed81
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-08-12 01:16:52 +0200

    make construktor of PackageService singelton private

--- src/services/PackageService.ts
+++ src/services/PackageService.ts
@@ -29,7 +29,7 @@ export default class PackagesService {
   private packageNamesList!: string[];
   private searchItemList!: SearchItem[];
 
-  constructor() {
+  private constructor() {
     this.packages = [];
     this.packageNamesList = [];
     this.searchItemList = [];
