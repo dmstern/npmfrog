ded1b076ec21a6ee28bee383d7fb4540a1e9741c
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-08-12 15:03:45 +0200

    move config search to new config-service

--- server/artifactory-service.js
+++ server/artifactory-service.js
@@ -1,26 +1,11 @@
 const axios = require("axios");
-const path = require("path");
-const homedir = require("os").homedir();
-const configFile = process.env.MOCK
-  ? ".sample.npmfrogrc.json"
-  : ".npmfrogrc.json";
-let config;
 
-try {
-  config = require(path.join(__dirname, "..", configFile));
-} catch(er1) {
-  try {
-    config = require(path.join(homedir, configFile));
-  } catch(er2) {
-    console.log('No config file found. Please create a config file as described in the README.md file or set environment variable MOCK=true to test the UI.');
-  }
-}
+const config = require('./config-service.js');
+const repoKey = config.artifactory.repoKey;
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 axios.defaults.baseURL = `${config.artifactory.baseURL}/artifactory/api/npm/`;
 axios.defaults.headers.common["Authorization"] = config.artifactory.apiKey;
-
-const repoKey = config.artifactory.repoKey;
 
 module.exports = {
   fetchPackages: async () => {
