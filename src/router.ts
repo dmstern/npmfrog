f0f8fb507bb756a4bb56c8167286042302c10522
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-08-20 00:57:01 +0200

    remove old import

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
-import About from './views/About.vue';
 import PackageDetail from './views/PackageDetail.vue';
 
 Vue.use(Router);
