559e9a6667264af59b1af77295c16921a371fdf7
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-08-20 08:28:50 +0200

    Fix mock / Format code

--- server/artifactory-service.js
+++ server/artifactory-service.js
@@ -1,14 +1,14 @@
 const axios = require("axios");
 const fs = require("fs-extra");
 const tar = require("tar");
-const showdown  = require('showdown');
+const showdown = require("showdown");
 
 const config = require("./config-service.js");
 const repoKey = config.artifactory.repoKey;
 const tmpDir = `${__dirname}/../tmp`;
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-const s = config.artifactory.https ? 's' : '';
+const s = config.artifactory.https ? "s" : "";
 axios.defaults.baseURL = `http${s}://${
   config.artifactory.baseURL
 }/artifactory/api/npm/${repoKey}`;
@@ -22,7 +22,7 @@ function readme2Html(readmeFile) {
   let readme;
   try {
     readme = fs.readFileSync(readmeFile);
-  } catch(error) {
+  } catch (error) {
     return null;
   }
   const converter = new showdown.Converter();
@@ -31,10 +31,10 @@ function readme2Html(readmeFile) {
 }
 
 function readMainCode(storageDir) {
-  const packageJson = require(`${storageDir}/package/package.json`);
+  const packageJson = require(`${storageDir}/package.json`);
   try {
-    return fs.readFileSync(`${storageDir}/package/${packageJson.main}`).toString();
-  } catch(error) {
+    return fs.readFileSync(`${storageDir}/${packageJson.main}`).toString();
+  } catch (error) {
     return null;
   }
 }
@@ -43,7 +43,7 @@ async function fetchPackages() {
   if (process.env.MOCK) {
     return new Promise((resolve, reject) => {
       resolve({
-        data: require("./mock/packages-all.json")
+        data: require(`${__dirname}/mock/packages-all.json`)
       });
     });
   }
@@ -51,68 +51,71 @@ async function fetchPackages() {
 }
 
 async function getPackageDetail({ scope, packageName }) {
-  const packageDetailResonse = process.env.MOCK ? 
-    await new Promise((resolve, reject) => {
-      resolve({
-        data: require(`./mock/fractal-menu-enhancer.json`)
-      });
-    }) : await axios.get(`/${name2url({ scope, packageName })}`);
+  const packageDetailResonse = process.env.MOCK
+    ? await new Promise((resolve, reject) => {
+        resolve({
+          data: require(`${__dirname}/mock/fractal-menu-enhancer.json`)
+        });
+      })
+    : await axios.get(`/${name2url({ scope, packageName })}`);
 
   const additionalCode = process.env.MOCK
-    ? await new Promise((resolve, reject) => { 
-      resolve({
-        readme: readme2Html(`${__dirname}/mock/fractal-menu-enhancer.readme.md`),
-        mainCode: readMainCode(`${__dirname}`),
-      });
-    })
-    : await new Promise(async (resolve, reject) => {
-      const packageDetail = packageDetailResonse.data;
-      const latestVersionResponse = await getDistTags({ scope, packageName });
-      const latestVersion = latestVersionResponse.data.latest;
-      const downloadUrl = packageDetail.versions[latestVersion].dist.tarball;
-      const storageDir = `${tmpDir}/${scope}/${packageName}/${latestVersion}`;
-      if (fs.existsSync(storageDir)) {
-        resolve(readAdditionalCode(storageDir));
-      } else {
-        axios
-        // Request package:
-        .request({
-          responseType: "arraybuffer",
-          url: downloadUrl,
-          method: "get",
-          headers: {
-            "Content-Type": "application/gzip"
-          }
-        })
-        // Store package in filesystem:
-        .then(result => {
-          fs.ensureDirSync(storageDir);
-          const outputFilename = `${storageDir}/${packageName}-${latestVersion}.tar.gz`;
-          fs.writeFileSync(outputFilename, result.data);
-          return outputFilename;
-        })
-        // Extract package:
-        .then(file => {
-          const cwd = storageDir;
-          return tar.x({ file, cwd }).then(() => cwd);
-        })
-        .then((dir) => {
-          resolve(readAdditionalCode(dir));
+    ? await new Promise((resolve, reject) => {
+        resolve({
+          readme: readme2Html(
+            `${__dirname}/mock/fractal-menu-enhancer.readme.md`
+          ),
+          mainCode: readMainCode(`${__dirname}/..`)
         });
-      }
-    });
+      })
+    : await new Promise(async (resolve, reject) => {
+        const packageDetail = packageDetailResonse.data;
+        const latestVersionResponse = await getDistTags({ scope, packageName });
+        const latestVersion = latestVersionResponse.data.latest;
+        const downloadUrl = packageDetail.versions[latestVersion].dist.tarball;
+        const storageDir = `${tmpDir}/${scope}/${packageName}/${latestVersion}`;
+        if (fs.existsSync(storageDir)) {
+          resolve(readAdditionalCode(storageDir));
+        } else {
+          axios
+            // Request package:
+            .request({
+              responseType: "arraybuffer",
+              url: downloadUrl,
+              method: "get",
+              headers: {
+                "Content-Type": "application/gzip"
+              }
+            })
+            // Store package in filesystem:
+            .then(result => {
+              fs.ensureDirSync(storageDir);
+              const outputFilename = `${storageDir}/${packageName}-${latestVersion}.tar.gz`;
+              fs.writeFileSync(outputFilename, result.data);
+              return outputFilename;
+            })
+            // Extract package:
+            .then(file => {
+              const cwd = storageDir;
+              return tar.x({ file, cwd }).then(() => cwd);
+            })
+            .then(dir => {
+              resolve(readAdditionalCode(dir));
+            });
+        }
+      });
 
-    Object.assign(packageDetailResonse.data, additionalCode);
-    return new Promise((resolve, reject) => {
-      resolve(packageDetailResonse);
-    });
+  Object.assign(packageDetailResonse.data, additionalCode);
+  return new Promise((resolve, reject) => {
+    resolve(packageDetailResonse);
+  });
 }
 
 function readAdditionalCode(storageDir) {
   return {
     readme: readme2Html(`${storageDir}/package/README.md`),
-    mainCode: readMainCode(storageDir),
-  }
+    mainCode: readMainCode(`${storageDir}/package`)
+  };
 }
 
 async function getDistTags({ scope, packageName }) {
