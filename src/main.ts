9c8c0e566ce6935393870b535180eca3197d6381
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-08-26 10:10:52 +0200

    Manage vue plugins

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import '@babel/polyfill';
 import '@mdi/font/css/materialdesignicons.css';
 import Vue from 'vue';
-import './plugins/vuetify';
+import './plugins/';
 import App from './App.vue';
 import router from '@/router';
-import VueSimpleSVG from 'vue-simple-svg';
-import VueHighlightJS from 'vue-highlightjs';
-import 'highlight.js/styles/atom-one-dark.css';
 
 Vue.config.productionTip = false;
-Vue.use(VueSimpleSVG);
-Vue.use(VueHighlightJS);
 
 new Vue({
   router,
---
+++ src/plugins/index.js
@@ -0,0 +1,3 @@
+import './vue-highlight';
+import './vue-simple-svg';
+import './vuetify';
