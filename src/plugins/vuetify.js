95077c5791658e5bbb93f854a70a9b8d7bcd7d96
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-08-27 12:30:46 +0200

    Change accent color to frog green

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import colors from 'vuetify/es5/util/colors';
 import 'vuetify/dist/vuetify.min.css';
 import 'font-awesome/css/font-awesome.min.css';
 
@@ -8,7 +9,7 @@ Vue.use(Vuetify, {
   theme: {
     primary: "#D63B09",
     secondary: "#FF7043",
-    accent: "#2196F3",
+    accent: colors.lightGreen.darken2,
     error: "#f44336",
     warning: "#ffeb3b",
     info: "#2196f3",
