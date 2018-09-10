2a8eedb019c2869655148837edf50612ef2593f7
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-09-11 01:40:19 +0200

    fix #22 fill howto publish page

--- server/config-service.ts
+++ server/config-service.ts
@@ -1,13 +1,14 @@
 import * as path from 'path';
 import * as os from 'os';
 import * as fs from 'fs-extra';
+import Config from '../types/Config';
 
 const homedir = os.homedir();
 
 const configFile = process.env.MOCK
   ? '.sample.npmfrogrc.json'
   : '.npmfrogrc.json';
-let config;
+let config: Config;
 
 try {
   config = fs.readJSONSync(path.join(__dirname, '..', '..', configFile));
