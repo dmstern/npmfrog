aae0ec6c1ab2f62d9b4190ca0f27fe35fbf0175e
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-10-08 15:39:01 +0200

    reformat cli.js to run under node

--- cli.js
+++ cli.js
@@ -5,40 +5,24 @@ const path = require('path');
 const pm2Config = require('./pm2.config');
 const port = pm2Config.serveUIStatic.env.PM2_SERVE_PORT;
 const logFiles = {
-  ui: path.join(
-    __dirname,
-    pm2Config.runServer.cwd || '',
-    pm2Config.runServer.log,
-  ),
-  server: path.join(
-    __dirname,
-    pm2Config.serveUIStatic.cwd || '',
-    pm2Config.serveUIStatic.log,
-  ),
+  ui: path.join(__dirname, pm2Config.runServer.cwd || '', pm2Config.runServer.log),
+  server: path.join(__dirname, pm2Config.serveUIStatic.cwd || '', pm2Config.serveUIStatic.log),
 };
 const programm = 'npmfrog';
 const firstArg = process.argv[2];
 const command = firstArg === 'stop' ? 'stop' : 'start';
 
-exec(
-  `npm ${command}`,
-  {
-    cwd: __dirname,
-  },
-  (error, stdout, stderr) => {
-    console.log(`${stdout}`);
-    console.error(`${stderr}`);
-    if (command === 'start') {
-      console.log(`Running npmFrog in background on http://localhost:${port}`);
-      console.log(`To stop npmFrog, run \`${programm} stop\``);
-      console.log(
-        `Logs can be found in ${logFiles.server} and ${logFiles.ui} .`,
-      );
-    } else if (command === 'stop') {
-      console.log(`Stopped npmFrog.`);
-    }
-    if (error !== null) {
-      console.error(`npmFrog error: ${error}`);
-    }
-  },
-);
+exec(`npm ${command}`, { cwd: __dirname }, (error, stdout, stderr) => {
+  console.log(`${stdout}`);
+  console.error(`${stderr}`);
+  if (command === 'start') {
+    console.log(`Running npmFrog in background on http://localhost:${port}`);
+    console.log(`To stop npmFrog, run \`${programm} stop\``);
+    console.log(`Logs can be found in ${logFiles.server} and ${logFiles.ui} .`);
+  } else if (command === 'stop') {
+    console.log(`Stopped npmFrog.`);
+  }
+  if (error !== null) {
+    console.error(`npmFrog error: ${error}`);
+  }
+});
