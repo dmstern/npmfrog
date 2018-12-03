55effaa467a466f4d02b8a0f3587f8e7b71785d5
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2018-12-03 23:13:40 +0100

    Adapt document.title to currrent route

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import Vue from 'vue';
-import Router from 'vue-router';
+import Router, { Route } from 'vue-router';
 import Home from './views/Home.vue';
 import PackageDetail from './views/PackageDetail.vue';
 import HowTo from './views/HowTo.vue';
 
 Vue.use(Router);
+
+const appName = 'npmFrog';
+const titleSuffix = ` | ${appName}`;
 
 export default new Router({
   routes: [
@@ -12,16 +15,33 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+      meta: {
+        getTitle: () => {
+          return appName;
+        },
+      },
     },
     {
       path: '/package/:scope?/:packageName',
       name: 'packageDetail',
       component: PackageDetail,
+      meta: {
+        getTitle: (route: Route) => {
+          return `${route.params.scope ? `${route.params.scope}/` : ''}${
+            route.params.packageName
+          }${titleSuffix}`;
+        },
+      },
     },
     {
       path: '/howto/',
       name: 'howto',
       component: HowTo,
+      meta: {
+        getTitle: (route: Route) => {
+          return `HowTo${titleSuffix}`;
+        },
+      },
     },
   ],
 });
